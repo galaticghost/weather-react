6c852092d1c4ef1fb2a56cce566630ce33437676
Arthur Suse <galaticghost@gmail.com>
2026-04-29 12:44:07 -0300

    Localização da timezone feita

--- src/Components/Timezone.tsx
+++ src/Components/Timezone.tsx
@@ -16,64 +16,64 @@ interface Props {
     timezone: string;
     onChange: (ChangeEventHandler<HTMLSelectElement>);
 }
-/*TODO TRADUÇÃO DAS TIMEZONES*/
-const options: Array<ContinentTimezones> = [
-    {
-        continent: "América Latina", timezones: [
-            { value: "America%2FSao_Paulo", label: "America/São Paulo" },
-            { value: "America%2FBuenos_Aires", label: "America/Buenos Aires" },
-            { value: "America%2FSantiago", label: "America/Santiago" },
-            { value: "America%2FLima", label: "America/Lima" }]
-    },
-    {
-        continent: "América do Norte", timezones: [
-            { value: "America%2FNew_York", label: "America/New York" },
-            { value: "America%2FChicago", label: "America/Chicago" },
-            { value: "America%2FDenver", label: "America/Denver" },
-            { value: "America%2FLos_Angeles", label: "America/Los Angeles" },
-            { value: "America%2FToronto", label: "America/Toronto" },
-            { value: "America%2FVancouver", label: "America/Vancouver" },
-            { value: "America%2FMexico_City", label: "America/Mexico City" },]
-    },
-
-    {
-        continent: "Europa", timezones: [
-            { value: "Europe%2FLondon", label: "Europe/London" },
-            { value: "Europe%2FParis", label: "Europe/Paris" },
-            { value: "Europe%2FBerlin", label: "Europe/Berlin" },
-            { value: "Europe%2FMadrid", label: "Europe/Madrid" },
-            { value: "Europe%2FRome", label: "Europe/Rome" },
-            { value: "Europe%2FMoscow", label: "Europe/Moscow" },]
-    },
-
-    {
-        continent: "Ásia", timezones: [
-            { value: "Asia%2FDubai", label: "Asia/Dubai" },
-            { value: "Asia%2FKolkata", label: "Asia/Kolkata" },
-            { value: "Asia%2FShanghai", label: "Asia/Shanghai" },
-            { value: "Asia%2FTokyo", label: "Asia/Tokyo" },
-            { value: "Asia%2FSeoul", label: "Asia/Seoul" },
-            { value: "Asia%2FSingapore", label: "Asia/Singapore" },
-            { value: "Asia%2FBangkok", label: "Asia/Bangkok" },]
-    },
-
-    {
-        continent: "Oceania", timezones: [
-            { value: "Australia%2FSydney", label: "Australia/Sydney" },
-            { value: "Pacific%2FAuckland", label: "Pacific/Auckland" }]
-    },
-
-    {
-        continent: "África", timezones: [
-            { value: "Africa%2FCairo", label: "Africa/Cairo" },
-            { value: "Africa%2FJohannesburg", label: "Africa/Johannesburg" },
-            { value: "Africa%2FLagos", label: "Africa/Lagos" },
-            { value: "Africa%2FNairobi", label: "Africa/Nairobi" },]
-    }
-];
-
 export default function Timezones({ timezone, onChange }: Props) {
     const { t } = useTranslation();
+
+    const options: Array<ContinentTimezones> = [
+        {
+            continent: t("timezone.latinAmerica"), timezones: [
+                { value: "America%2FSao_Paulo", label: "São Paulo" },
+                { value: "America%2FBuenos_Aires", label: "Buenos Aires" },
+                { value: "America%2FSantiago", label: "Santiago" },
+                { value: "America%2FLima", label: "Lima" },
+                { value: "America%2FMexico_City", label: "Mexico City" },]
+        },
+        {
+            continent: t("timezone.northAmerica"), timezones: [
+                { value: "America%2FNew_York", label: "New York" },
+                { value: "America%2FChicago", label: "Chicago" },
+                { value: "America%2FDenver", label: "Denver" },
+                { value: "America%2FLos_Angeles", label: "Los Angeles" },
+                { value: "America%2FToronto", label: "Toronto" },
+                { value: "America%2FVancouver", label: "Vancouver" },
+                ]
+        },
+
+        {
+            continent: t("timezone.europe"), timezones: [
+                { value: "Europe%2FLondon", label: "London" },
+                { value: "Europe%2FParis", label: "Paris" },
+                { value: "Europe%2FBerlin", label: "Berlin" },
+                { value: "Europe%2FMadrid", label: "Madrid" },
+                { value: "Europe%2FRome", label: "Rome" },
+                { value: "Europe%2FMoscow", label: "Moscow" },]
+        },
+
+        {
+            continent: t("timezone.asia"), timezones: [
+                { value: "Asia%2FDubai", label: "Dubai" },
+                { value: "Asia%2FKolkata", label: "Kolkata" },
+                { value: "Asia%2FShanghai", label: "Shanghai" },
+                { value: "Asia%2FTokyo", label: "Tokyo" },
+                { value: "Asia%2FSeoul", label: "Seoul" },
+                { value: "Asia%2FSingapore", label: "Singapore" },
+                { value: "Asia%2FBangkok", label: "Bangkok" },]
+        },
+
+        {
+            continent: t("timezone.oceania"), timezones: [
+                { value: "Australia%2FSydney", label: "Sydney" },
+                { value: "Pacific%2FAuckland", label: "Auckland" }]
+        },
+
+        {
+            continent: t("timezone.africa"), timezones: [
+                { value: "Africa%2FCairo", label: "Cairo" },
+                { value: "Africa%2FJohannesburg", label: "Johannesburg" },
+                { value: "Africa%2FLagos", label: "Lagos" },
+                { value: "Africa%2FNairobi", label: "Nairobi" },]
+        }
+    ];
 
     return (
         <div className="item">
